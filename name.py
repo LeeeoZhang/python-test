d05b8cf9746357dff930180bf3227f9f9342fac2
leo <leozhang621@gmail.com>
2017-10-08 21:20:02 +0800

    modified name

--- name.py
+++ name.py
@@ -1,15 +1,25 @@
 firstName = "leo"
 lastName = "cheung"
 fullName = firstName + ' ' + lastName
+famousPerson = 'Albert Einstein'
+spaceName = "\nleo cheung\t"
 
-message = "Hello, " + fullName + "!"
+message1 = "Hello, " + fullName + "!"
+message2 = famousPerson +' once said,"A person who never made a mistake never tried anything new."'
 upperName = fullName.upper()
 lowerName = fullName.lower()
 titleName = fullName.title()
 
-print(message)
+
+print(message1)
 print(upperName)
 print(lowerName)
 print(titleName)
 
+print(message2)
+
+print(spaceName)
+print(spaceName.rstrip())
+print(spaceName.lstrip())
+print(spaceName.strip())
 
